0658a4b1cfde77e8ad0dfd898a241c1f33e8658d
SNWStaff <55186225+SNWStaff@users.noreply.github.com>
2019-12-04 16:36:34 +0100

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,7 @@ module.exports = {
     dependency: {
         platforms: {
             android: {
+                "packageImportPath": "import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage",
                 "packageInstance": "new ReactNativePushNotificationPackage()"
             } 
         }
